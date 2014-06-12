83f248c4d7d30fb116fddd40e3f0a9e6ee7af48d
AntonShelest <shelest.anton@gmail.com>
2014-06-12 13:45:02 +0300

    Model Classes
    Added new classes from ERD into our model

---
+++ FancyImageUploader/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FancyImageUploader.Models.ERD
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int FolderId { get; set; }
+    }
+}
---
+++ FancyImageUploader/Models/FolderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FancyImageUploader.Models
+{
+    public class FolderModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public int SupFolderId { get; set; }
+        public int Rights { get; set; }
+        public string Link { get; set; }
+    }
+}
